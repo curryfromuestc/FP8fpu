367d3e74879af4e351de3123a243e7ee3ba4a43e
curryfromuestc <curryfromuestc@gmail.com>
2025-06-03 11:23:32 +0800

    修改部分位宽

--- src/main/scala/fpu/core/FiveBitsAdder.scala
+++ src/main/scala/fpu/core/FiveBitsAdder.scala
@@ -8,19 +8,19 @@ class FiveBitsAdder extends Module {
     val io = IO(new Bundle {
         val a = Input(UInt(4.W))
         val b = Input(UInt(4.W))
-        val out = Output(SInt(6.W))
+        val out = Output(SInt(5.W))
     })
 
-    val aNobias = Wire(SInt(6.W))
-    val bNobias = Wire(SInt(6.W))
+    val aNobias = Wire(SInt(4.W))
+    val bNobias = Wire(SInt(4.W))
     
-    aNobias := Cat(0.U(1.W), io.a).asSInt - 7.S
-    bNobias := Cat(0.U(1.W), io.b).asSInt - 7.S
+    aNobias := io.a.asSInt - 7.S
+    bNobias := io.b.asSInt - 7.S
 
     // printf("aNobias: %d\n", aNobias)
     // printf("bNobias: %d\n", bNobias)
 
-    io.out := aNobias + bNobias
+    io.out := aNobias +& bNobias
 }
 
 object FiveBitsAdder extends App {
